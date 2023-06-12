7c0fb2187cf40e2ac75b5fe4c89ed3e03f75accd
henrybell <henrybell@google.com>
2023-06-12 16:39:05 +0000

    Checkpoint commit

--- terraform/cloud-functions/per-project/test/go.mod
+++ terraform/cloud-functions/per-project/test/go.mod
@@ -3,16 +3,19 @@ module github.com/cloudspannerecosystem/autoscaler/terraform/cloud-functions/per
 go 1.21
 
 require (
+	cloud.google.com/go/scheduler v1.10.0
+	cloud.google.com/go/spanner v1.46.0
 	github.com/gruntwork-io/terratest v0.43.0
 	github.com/stretchr/testify v1.8.4
 )
 
 require (
-	cloud.google.com/go v0.105.0 // indirect
-	cloud.google.com/go/compute v1.12.1 // indirect
-	cloud.google.com/go/compute/metadata v0.2.1 // indirect
-	cloud.google.com/go/iam v0.7.0 // indirect
-	cloud.google.com/go/storage v1.27.0 // indirect
+	cloud.google.com/go v0.110.0 // indirect
+	cloud.google.com/go/compute v1.19.3 // indirect
+	cloud.google.com/go/compute/metadata v0.2.3 // indirect
+	cloud.google.com/go/iam v0.13.0 // indirect
+	cloud.google.com/go/longrunning v0.4.1 // indirect
+	cloud.google.com/go/storage v1.28.1 // indirect
 	github.com/agext/levenshtein v1.2.3 // indirect
 	github.com/apparentlymart/go-textseg/v13 v13.0.0 // indirect
 	github.com/aws/aws-sdk-go v1.44.122 // indirect
@@ -33,9 +36,10 @@ require (
 	github.com/google/gnostic v0.5.7-v3refs // indirect
 	github.com/google/go-cmp v0.5.9 // indirect
 	github.com/google/gofuzz v1.1.0 // indirect
+	github.com/google/s2a-go v0.1.4 // indirect
 	github.com/google/uuid v1.3.0 // indirect
-	github.com/googleapis/enterprise-certificate-proxy v0.2.0 // indirect
-	github.com/googleapis/gax-go/v2 v2.7.0 // indirect
+	github.com/googleapis/enterprise-certificate-proxy v0.2.3 // indirect
+	github.com/googleapis/gax-go/v2 v2.10.0 // indirect
 	github.com/gruntwork-io/go-commons v0.8.0 // indirect
 	github.com/hashicorp/errwrap v1.0.0 // indirect
 	github.com/hashicorp/go-cleanhttp v0.5.2 // indirect
@@ -69,19 +73,21 @@ require (
 	github.com/urfave/cli v1.22.2 // indirect
 	github.com/zclconf/go-cty v1.9.1 // indirect
 	go.opencensus.io v0.24.0 // indirect
-	golang.org/x/crypto v0.1.0 // indirect
-	golang.org/x/net v0.8.0 // indirect
-	golang.org/x/oauth2 v0.1.0 // indirect
-	golang.org/x/sys v0.6.0 // indirect
-	golang.org/x/term v0.6.0 // indirect
-	golang.org/x/text v0.8.0 // indirect
+	golang.org/x/crypto v0.9.0 // indirect
+	golang.org/x/net v0.10.0 // indirect
+	golang.org/x/oauth2 v0.8.0 // indirect
+	golang.org/x/sys v0.8.0 // indirect
+	golang.org/x/term v0.8.0 // indirect
+	golang.org/x/text v0.9.0 // indirect
 	golang.org/x/time v0.0.0-20220210224613-90d013bbcef8 // indirect
 	golang.org/x/xerrors v0.0.0-20220907171357-04be3eba64a2 // indirect
-	google.golang.org/api v0.103.0 // indirect
+	google.golang.org/api v0.125.0 // indirect
 	google.golang.org/appengine v1.6.7 // indirect
-	google.golang.org/genproto v0.0.0-20221201164419-0e50fba7f41c // indirect
-	google.golang.org/grpc v1.51.0 // indirect
-	google.golang.org/protobuf v1.28.1 // indirect
+	google.golang.org/genproto v0.0.0-20230530153820-e85fd2cbaebc // indirect
+	google.golang.org/genproto/googleapis/api v0.0.0-20230530153820-e85fd2cbaebc // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20230530153820-e85fd2cbaebc // indirect
+	google.golang.org/grpc v1.55.0 // indirect
+	google.golang.org/protobuf v1.30.0 // indirect
 	gopkg.in/inf.v0 v0.9.1 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
--- terraform/cloud-functions/per-project/test/per_project_test.go
+++ terraform/cloud-functions/per-project/test/per_project_test.go
@@ -17,37 +17,72 @@
 package test
 
 import (
+	"context"
+	"fmt"
 	"testing"
+	"time"
 
+	scheduler "cloud.google.com/go/scheduler/apiv1beta1"
+	schedulerpb "cloud.google.com/go/scheduler/apiv1beta1/schedulerpb"
+	instance "cloud.google.com/go/spanner/admin/instance/apiv1"
+	instancepb "cloud.google.com/go/spanner/admin/instance/apiv1/instancepb"
+
+	logger "github.com/gruntwork-io/terratest/modules/logger"
+	retry "github.com/gruntwork-io/terratest/modules/retry"
+	terraform "github.com/gruntwork-io/terratest/modules/terraform"
 	test_structure "github.com/gruntwork-io/terratest/modules/test-structure"
 
-	"github.com/gruntwork-io/terratest/modules/terraform"
-	"github.com/stretchr/testify/assert"
+	assert "github.com/stretchr/testify/assert"
 )
+
+const (
+	preScalingProcessingUnits   = 100
+	postScalingProcessingUnits  = 200
+	spannerTestInstanceTfOutput = "spanner_test_instance_name"
+	schedulerJobTfOutput        = "scheduler_job_id"
+	projectId                   = "placeholder"
+	region                      = "us-central1"
+	zone                        = "us-central1-a"
+)
+
+func waitForSpannerProcessingUnits(t *testing.T, instanceAdmin *instance.InstanceAdminClient, instanceId string, targetProcessingUnits int32, retries int, sleepBetweenRetries time.Duration) {
+	ctx := context.Background()
+	status := fmt.Sprintf("Wait for instance to reach %d (PUs)...", targetProcessingUnits)
+	message := retry.DoWithRetry(
+		t,
+		status,
+		retries,
+		sleepBetweenRetries,
+		func() (string, error) {
+			spannerInstanceReq := &instancepb.GetInstanceRequest{
+				Name: instanceId,
+			}
+			spannerInstance, err := instanceAdmin.GetInstance(ctx, spannerInstanceReq)
+			assert.Nil(t, err)
+			assert.NotNil(t, spannerInstance)
+			processingUnits := spannerInstance.GetProcessingUnits()
+			if processingUnits != targetProcessingUnits {
+				return "", fmt.Errorf("Currently %d PUs", processingUnits)
+			}
+			return "Spanner instance reached target PUs", nil
+		},
+	)
+	logger.Log(t, message)
+}
 
 func TestPerProjectEndToEndDeployment(t *testing.T) {
 
-	const projectID = "placeholder"
-	const region = "us-central1"
-	const zone = "us-central1-a"
-
-	t.Parallel()
-
 	terraformDir := "../"
-
-	defer test_structure.RunTestStage(t, "teardown", func() {
-		terraformOptions := test_structure.LoadTerraformOptions(t, terraformDir)
-		terraform.Destroy(t, terraformOptions)
-	})
 
 	test_structure.RunTestStage(t, "setup", func() {
 		terraformOptions := &terraform.Options{
 			TerraformDir: terraformDir,
 			Vars: map[string]interface{}{
-				"project_id":             projectID,
+				"project_id":             projectId,
 				"region":                 region,
 				"zone":                   zone,
 				"terraform_spanner_test": "true",
+				"terraform_spanner_test_processing_units": preScalingProcessingUnits,
 			},
 		}
 
@@ -55,9 +90,14 @@ func TestPerProjectEndToEndDeployment(t *testing.T) {
 		terraform.Init(t, terraformOptions)
 	})
 
+	defer test_structure.RunTestStage(t, "teardown", func() {
+		terraformOptions := test_structure.LoadTerraformOptions(t, terraformDir)
+		terraform.Destroy(t, terraformOptions)
+	})
+
 	test_structure.RunTestStage(t, "import", func() {
 		terraformOptions := test_structure.LoadTerraformOptions(t, terraformDir)
-		terraformArgs := []string{"module.scheduler.google_app_engine_application.app", projectID}
+		terraformArgs := []string{"module.scheduler.google_app_engine_application.app", projectId}
 		terraformArgsFormatted := append(terraform.FormatArgs(terraformOptions, "-input=false"), terraformArgs...)
 		terraformCommand := append([]string{"import"}, terraformArgsFormatted...)
 		terraform.RunTerraformCommand(t, terraformOptions, terraformCommand...)
@@ -69,7 +109,35 @@ func TestPerProjectEndToEndDeployment(t *testing.T) {
 	})
 
 	test_structure.RunTestStage(t, "validate", func() {
-		//terraformOptions := test_structure.LoadTerraformOptions(t, terraformDir)
-		assert.True(t, true)
+		terraformOptions := test_structure.LoadTerraformOptions(t, terraformDir)
+		ctx := context.Background()
+
+		// Create Spanner Admin client
+		spannerTestInstanceName := terraform.Output(t, terraformOptions, spannerTestInstanceTfOutput)
+		instanceAdmin, err := instance.NewInstanceAdminClient(ctx)
+		assert.Nil(t, err)
+		assert.NotNil(t, instanceAdmin)
+		defer instanceAdmin.Close()
+
+		// Create Scheduler client
+		schedulerJobId := terraform.Output(t, terraformOptions, schedulerJobTfOutput)
+		schedulerClient, err := scheduler.NewCloudSchedulerClient(ctx)
+		assert.Nil(t, err)
+		assert.NotNil(t, schedulerClient)
+		defer schedulerClient.Close()
+
+		// Get Scheduler job
+		schedulerJobReq := &schedulerpb.GetJobRequest{
+			Name: schedulerJobId,
+		}
+		schedulerJob, err := schedulerClient.GetJob(ctx, schedulerJobReq)
+		assert.Nil(t, err)
+		assert.NotNil(t, schedulerJob)
+
+		// Wait for Spanner to report initial processing units
+		spannerTestInstanceId := fmt.Sprintf("projects/%s/instances/%s", projectId, spannerTestInstanceName)
+		waitForSpannerProcessingUnits(t, instanceAdmin, spannerTestInstanceId, preScalingProcessingUnits, 10, time.Second*10)
+
+		//fmt.Printf("%#v \n", schedulerJob)
 	})
 }
